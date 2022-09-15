605e8b72c7bbf539368e56442bbd159654bb93bb
Adriana-Kornreuter <110978913+Adriana-Kornreuter@users.noreply.github.com>
2022-09-14 22:12:20 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -7,7 +7,7 @@ image01 = Image.open('logo.jpg')
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("Sascar Tecnologia e Segurança Automotiva S/A")
 
-elif choice == "Inserir_Figura":
+if choice == "Inserir_Figura":
     st.image(image01, width=400, caption='Logo') 
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
