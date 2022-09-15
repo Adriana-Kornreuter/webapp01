c66a8902b36fc5272f079f086b9bbb0d9fa035e7
Adriana-Kornreuter <110978913+Adriana-Kornreuter@users.noreply.github.com>
2022-09-14 23:04:58 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -27,26 +27,26 @@ choice = st.sidebar.selectbox("Menu de Opções",menu)
 st.sidebar.write("Texto Side Bar")
     
 #if choice == "Texto_Colunas":       
-    st.subheader("Texto formatado em colunas")
-    st.write("Veja a seguir uma opção de formatação em colunas")    
-    cols01 = st.columns(2)    
-    cols01[0].write('Texto da Coluna 01')
-    cols01[1].write('Texto da Coluna 02')
+    #st.subheader("Texto formatado em colunas")
+    #st.write("Veja a seguir uma opção de formatação em colunas")    
+    #cols01 = st.columns(2)    
+    #cols01[0].write('Texto da Coluna 01')
+    #cols01[1].write('Texto da Coluna 02')
 #elif choice == "Texto_Markdown":
-    st.subheader("Texto Markdown")
-    st.write("Veja a seguir opção de formatação de texto Markdown")
-    st.markdown(
-    """
+    #st.subheader("Texto Markdown")
+    #st.write("Veja a seguir opção de formatação de texto Markdown")
+    #st.markdown(
+    #"""
     ## *Alguns sites referências*:    
-    - [Streamlit: hello world](https://calmcode.io/streamlit/hello-world.html)
-    - [:star: Streamlit emoji shortcodes](https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlitapp.com/)
-    - [Layouts and Containers](https://docs.streamlit.io/library/api-reference/layout)
+  #  - [Streamlit: hello world](https://calmcode.io/streamlit/hello-world.html)
+   # - [:star: Streamlit emoji shortcodes](https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlitapp.com/)
+   # - [Layouts and Containers](https://docs.streamlit.io/library/api-reference/layout)
    
     ##### CRONOGRAMA
-    DIA | CH HORÁRIA | CONTEÚDO
+    #DIA | CH HORÁRIA | CONTEÚDO
     :---------: | :------: | :-------:
-    Dia 1 de 2 | ?h | ? a ?
-    Dia 2 de 2 | ?h | ? a ?
+    #Dia 1 de 2 | ?h | ? a ?
+   # Dia 2 de 2 | ?h | ? a ?
     """
     )
     
